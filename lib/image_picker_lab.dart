0223c37b8085f823cc606d53c9c04fc2060c2c10
Sergey Solodukhin <soloss@yandex.ru>
2020-01-21 22:42:51 +0300

    upload image with MultipartRequest

--- lib/image_picker_lab.dart
+++ lib/image_picker_lab.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
+const String url = 'http://192.168.43.205:8888/media';
 
 class ImagePickerLabPage extends StatefulWidget {
   @override
@@ -30,6 +34,27 @@ class _ImagePickerLabPageState extends State<ImagePickerLabPage> {
         tooltip: 'Pick Image',
         child: Icon(Icons.add_a_photo),
       ),
+      persistentFooterButtons: <Widget>[
+        FlatButton(
+          child: Text('upload'),
+          onPressed: _upload,
+        )
+      ],
     );
+  }
+
+  void _upload() async {
+    if (_image == null) return;
+    final fileName = _image.path.split('/').last;
+    final length = await _image.length();
+    final request = http.MultipartRequest('POST', Uri.parse(url))
+      ..files.add(http.MultipartFile(
+        'avatar',
+        _image.openRead(),
+        length,
+        filename: fileName,
+      ));
+    final response = await http.Response.fromStream(await request.send());
+    print("Result: ${response.statusCode} ${response.reasonPhrase}");
   }
 }
