8e31743f87b0d7cce6924e996f80dad9fdf123c3
Sergey Solodukhin <soloss@yandex.ru>
2020-04-14 19:38:30 +0300

    play with location service on google maps

--- lib/google_maps_lab.dart
+++ lib/google_maps_lab.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'services/location_service.dart';
 
 class GoogleMapsLabPage extends StatefulWidget {
   @override
@@ -7,53 +11,39 @@ class GoogleMapsLabPage extends StatefulWidget {
 }
 
 class GoogleMapsLabPageState extends State<GoogleMapsLabPage> {
-  final preset = [
-    {"id": "mountain-view", "lat": 37.421512, "lng": -122.084101},
-    {"id": "san-bruno", "lat": 37.62816, "lng": -122.426491},
-    {"id": "san-francisco", "lat": 37.789972, "lng": -122.390013},
-    {"id": "sunnyvale", "lat": 37.403694, "lng": -122.031583}
-  ];
+  GoogleMapController _mapController;
+  StreamSubscription _streamSubscription;
+  double _zoom = 9.0;
 
-  String _selectedOffice = '';
-
-  Set<Marker> _buildMarkers() {
-    return preset.map((office) {
-      return Marker(
-          markerId: MarkerId(office['id']),
-          position: LatLng(office['lat'], office['lng']),
-          icon: office['id'] == _selectedOffice
-              ? BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
-              : BitmapDescriptor.defaultMarker,
-          consumeTapEvents: true,
-          onTap: () {
-            setState(() {
-              _selectedOffice = office['id'];
-            });
-          });
-    }).toSet();
+  @override
+  void initState() {
+    _streamSubscription = LocationService().locationStream.listen((event) {
+      print('${event.latitude} ${event.longitude}');
+      _mapController.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(event.latitude, event.longitude), zoom: _zoom)));
+    });
+    super.initState();
   }
 
-  Set<Polyline> _buildPolylines() {
-    return List.of([
-      Polyline(
-        polylineId: PolylineId('abcde1234'),
-        patterns: <PatternItem>[PatternItem.dash(5), PatternItem.gap(5)],
-        width: 2,
-        color: Colors.red,
-        points: preset.map((office) {
-          return LatLng(office['lat'], office['lng']);
-        }).toList(),
-      )
-    ]).toSet();
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: LatLng(37.7, -122.2), zoom: 9.0),
-        markers: _buildMarkers(),
-        polylines: _buildPolylines(),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: CameraPosition(target: LatLng(37.7, -122.2), zoom: _zoom),
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+        onCameraMove: (CameraPosition pos) {
+          _zoom = pos.zoom;
+        },
       ),
     );
   }
